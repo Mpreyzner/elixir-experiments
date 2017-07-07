03f0b4c91f53b718c08e44c16262b16195418e52
Mpreyzner <maria.preyzner@gmail.com>
2017-07-07 20:32:24 +0200

    Add test routes

--- web/router.ex
+++ web/router.ex
@@ -19,6 +19,7 @@ defmodule HelloPhoenix.Router do
     get "/", PageController, :index
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
+    resources "/user", UserController, except: [:delete]
   end
 
   # Other scopes may use custom stacks.
